baf984d2793bdc55a7e229a064adc5e74ed0e262
@iOSRunner <@ericguo1213@163.com>
2017-05-17 20:47:59 +0800

    test demo app

--- DTTrackerDemo/ViewController.m
+++ DTTrackerDemo/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <DTTracker/DTTrackManager.h>
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    DTTrackManager *tracker = [[DTTrackManager alloc] init];
+    [tracker trackEvent:@"viewDidLoad"];
+    
 }
 
 
