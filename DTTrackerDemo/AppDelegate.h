d4b0bcc495d6dec326edd81268cc3deebab2f53b
@iOSRunner <@ericguo1213@163.com>
2017-05-17 20:35:03 +0800

    init test  project

---
+++ DTTrackerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DTTrackerDemo
+//
+//  Created by Wanlei on 2017/5/17.
+//  Copyright © 2017年 organizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DTTrackerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DTTrackerDemo
+//
+//  Created by Wanlei on 2017/5/17.
+//  Copyright © 2017年 organizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
